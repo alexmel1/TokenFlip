592bbb45799d64a872ad549a4ae6d6ae65bb0381
PolkaMela <51043063+alexmel1@users.noreply.github.com>
2026-03-25 10:31:47 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,27 @@
-import type { Metadata } from "next";
-import { Providers } from "./providers";
-import "@coinbase/onchainkit/styles.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google'; 
+import './globals.css';
+import { Providers } from './providers'; 
+
 
 export const metadata: Metadata = {
-  title: "TokenFlip",
+  title: 'TokenFlip', 
   other: {
-    'base:app_id': '69c0b79476e804b2a67a9f96',
+    'base:app:id': '0xac0107e0e84b70e0a0a5d2eb3d0f0eb9',
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, backgroundColor: '#020617' }}>
+      <head>
+        <meta name="talentapp:project_verification" content="8e68c0e710831f2d87a50614438d623bad71d1bbc3cce77ecbeab11b7a893415e9b2b3335f2718ccfc743a6bba9ecd42f8dc4cebc33017e8178a2d0d8d0ef496" />
+      </head>
+      <body style={{ display: 'flex', flexDirection: 'column', background: '#020617' }}>
         <Providers>{children}</Providers>
       </body>
     </html>
