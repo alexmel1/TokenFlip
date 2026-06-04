0ff885992a242be5c1b74862191d439b8c15148f
PolkaMela <51043063+alexmel1@users.noreply.github.com>
2026-06-04 22:39:14 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-
-
 import type { Metadata } from "next";
 import { Providers } from "./providers";
 import "@coinbase/onchainkit/styles.css";
@@ -7,18 +5,25 @@ import "@coinbase/onchainkit/styles.css";
 export const metadata: Metadata = {
   title: "TokenFlip",
   other: {
-    'base:app_id': '0xac0107e0e84b70e0a0a5d2eb3d0f0eb9',
+    "impact-site-verification":
+      "937fd3cc-f4f8-4dd2-9f80-0ef3da8cd569",
+
+    "talentapp:project_verification":
+      "8e68c0e710831f2d87a50614438d623bad71d1bbc3cce77ecbeab11b7a893415e9b2b3335f2718ccfc743a6bba9ecd42f8dc4cebc33017e8178a2d0d8d0ef496",
+
+    "base:app_id":
+      "0xac0107e0e84b70e0a0a5d2eb3d0f0eb9",
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <head> 
-        <meta name='impact-site-verification' value='937fd3cc-f4f8-4dd2-9f80-0ef3da8cd569' />
-        <meta name="talentapp:project_verification" content="8e68c0e710831f2d87a50614438d623bad71d1bbc3cce77ecbeab11b7a893415e9b2b3335f2718ccfc743a6bba9ecd42f8dc4cebc33017e8178a2d0d8d0ef496" />
-      </head>
-      <body style={{ margin: 0, backgroundColor: '#020617' }}>
+      <body style={{ margin: 0, backgroundColor: "#020617" }}>
         <Providers>{children}</Providers>
       </body>
     </html>
